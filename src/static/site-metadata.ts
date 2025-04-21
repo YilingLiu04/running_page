8e5ea5fca6b851558d1b60d2602f846919529314
root <root@Liuyiling>
2025-04-21 23:25:44 +0800

    update

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,7 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://yiling.run',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
@@ -24,13 +24,10 @@ const data: ISiteMetadataResult = {
       name: 'Summary',
       url: `${getBasePath()}/summary`,
     },
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
+
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/yiling615/running_page',
     },
   ],
 };
